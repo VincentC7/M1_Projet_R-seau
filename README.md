0dfc7b0db86148ed2d808f46a2ef213d76332ce5
Vincent <vincent.choquert0@gmail.com>
2021-05-14 20:55:00 +0200

    Deuxieme version server HTPP
    chargement des ressource
    multithread

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 ##Projet de réseau M1
 
 Ce projet à pour objectif de recréer le protocol HTTP
+
+###Utilisation
+Dans Ressources_exte figure l'ensemble des ressources que va utiliser notre serveur. 
+Si vous souhaitez définir vos sites il faut faire une copie de conf_exemple.properties nommée "conf.properties" modifier les infos exitantes.
+
+###Hosts
+Pour que vos noms de domaines soient reconnus en local il faut modifier votre fichier hosts (voir google comment faire).  
+Pour chaque site aujouter :
+  * 127.0.0.1 www.exemple.fr 
+
+
--- src/main/java/fr/ul/miage/projetreseau/Main.java
+++ src/main/java/fr/ul/miage/projetreseau/Main.java
@@ -1,9 +1,34 @@
 package fr.ul.miage.projetreseau;
 
+import fr.ul.miage.projetreseau.service.GeneralProperties;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Init");
+        try {
+            //Site 1
+            String serv1_domain = GeneralProperties.getPropertie("SERVERS[0].domain");
+            String serv1_root = GeneralProperties.getPropertie("SERVERS[0].root");
+            String serv1_port = GeneralProperties.getPropertie("SERVERS[0].port");
+            new HTTPServer(new File(serv1_root), Integer.parseInt(serv1_port), serv1_domain);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //Site 2
+        String serv2_domain = GeneralProperties.getPropertie("SERVERS[1].domain");
+        String serv2_root = GeneralProperties.getPropertie("SERVERS[1].root");
+        String serv2_port = GeneralProperties.getPropertie("SERVERS[1].port");
+        try {
+            new HTTPServer(new File(serv2_root), Integer.parseInt(serv2_port), serv2_domain);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
 }
